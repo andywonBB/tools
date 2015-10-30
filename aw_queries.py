30f44f1feffee752b8abf2afdea9171ebb81929e
Andy Won <andy.won@birchbox.com>
2015-10-30 15:54:54 -0400

    queries

--- aw_queries.py
+++ aw_queries.py
@@ -1,17 +1,6 @@
 hive_session_analytics = """
 
-    ADD JAR /home/klai/projects/warehouse_and_analytics/warehouse_crons/lib/HiveSwarm-1.0-SNAPSHOT.jar;
-    create temporary function user_agent_parser as 'com.livingsocial.hive.udf.UserAgentParser';
-    ADD JAR /home/andywon/projects/udfs/brickhouse/target/brickhouse-0.7.1-SNAPSHOT.jar;
-    CREATE TEMPORARY FUNCTION collect AS 'brickhouse.udf.collect.CollectUDAF';
-    CREATE TEMPORARY FUNCTION to_json AS 'brickhouse.udf.json.ToJsonUDF';
-    INSERT OVERWRITE LOCAL DIRECTORY '/home/andywon/projects/session_analytics_test/%s'
-    ROW FORMAT DELIMITED
-    FIELDS TERMINATED BY '\\t'
-    LINES TERMINATED BY '\\n'
-
-    ### event_raw backdates to 2014-02-01
-    SELECT ws.visitor_id, ws.visit_id, to_date(to_utc_timestamp(from_unixtime(ws.visit_start), 'America/New_York')) as dt, ws.visit_start, ws.visit_end, ws.num_pages, ws.customer_ids[0], ws.landing_url, ws.exit_url,
+    SELECT ws.visitor_id, ws.visit_id, to_utc_timestamp(from_unixtime(ws.visit_start), 'America/New_York') as visit_start, to_utc_timestamp(from_unixtime(ws.visit_end), 'America/New_York') as visit_end, ws.num_pages, ws.customer_ids[0], ws.landing_url, ws.exit_url,
     ws.ecom_session_revenue, ws.rebillable_sub_session_revenue, ws.gift_sub_session_revenue, ws.giftcard_session_revenue, ws.ecom_session_units, ws.rebillable_sub_session_units, ws.gift_sub_session_units, ws.giftcard_session_units,
     CASE
         WHEN source = 'paid' THEN
@@ -24,8 +13,14 @@ hive_session_analytics = """
         END)
     ELSE source
     END as source,
-    pv.ua_family,
-    pv.os_family,
+    ws.extensible_param,
+    ws.remote_ip,
+    ws.zipcode,
+    ws.city,
+    ws.dmacode,
+    ws.increment_ids,
+    pv.browser,
+    pv.client_os,
     pv.device,
     er.unique_pdp_views,
     er.unique_catalog_views,
@@ -34,6 +29,8 @@ hive_session_analytics = """
     er.shop_home,
     er.brand_index,
     er.cart_viewed,
+    er.box_viewed,
+    er.samples_viewed,
     er.checkout_viewed,
     er.bbplus_addon,
     exp.experiments
@@ -49,24 +46,28 @@ hive_session_analytics = """
     MAX(CASE WHEN event = 'view' AND context = 'shop-home' THEN '1-true' ELSE '0-false' END) as shop_home,
     MAX(CASE WHEN event = 'view' AND context = 'brand-index' THEN '1-true' ELSE '0-false' END) as brand_index,
     MAX(CASE WHEN event = 'view' AND context = 'cart' THEN '1-true' ELSE '0-false' END) as cart_viewed,
-    MAX(CASE WHEN event = 'view' AND context RLIKE 'checkout*' THEN '1-true' ELSE '0-false' END) as checkout_viewed,
+    MAX(CASE WHEN event = 'view' AND context = 'bxdp' THEN '1-true' ELSE '0-false' END) as box_viewed,
+    MAX(CASE WHEN event = 'view' AND context = 'samples' THEN '1-true' ELSE '0-false' END) as samples_viewed,
+    MAX(CASE WHEN event = 'view' AND context in ('checkout', 'checkout-success', 'express-checkout') THEN '1-true' ELSE '0-false' END) as checkout_viewed,
     MAX(CASE WHEN context = 'pdp' AND get_json_object(properties, '$.title') = 'Reserve Now' THEN '1-true' ELSE '0-false' END) as bbplus_addon
     FROM event_raw ev
     WHERE dt >= %s and dt <= %s
+    AND context in ('pdp', 'catalog-index', 'serp', 'bdp', 'shop_home', 'brand_index', 'cart', 'checkout', 'checkout-success', 'express-checkout', 'samples', 'bxdp')
+    AND event in ('view', 'click')
     GROUP BY visitor_id, visit_id
     ) er 
     ON ws.visitor_id = er.visitor_id AND ws.visit_id = er.visit_id
     JOIN (
     SELECT 
     visitor_id,
-    MAX(user_agent_parser(user_agent, 'ua_family')) AS ua_family,
-    MAX(user_agent_parser(user_agent, 'os_family')) AS os_family,
+    MAX(user_agent_parser(user_agent, 'ua_family')) AS browser,
+    MAX(user_agent_parser(user_agent, 'os_family')) AS client_os,
     MAX(user_agent_parser(user_agent, 'device')) AS device
     FROM pageviews_raw
     WHERE dt >= %s and dt <= %s
     GROUP BY visitor_id
     ) pv ON ws.visitor_id = pv.visitor_id
-    JOIN (
+    LEFT JOIN (
     SELECT visitor_id, visit_id, to_json(collect(event, get_json_object(properties, '$.variant'))) as experiments
     FROM event_raw
     WHERE context = 'experiment'

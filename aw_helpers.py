5c02a56d9e0981aabaf713565773f1660e62c937
Andy Won <andy.won@birchbox.com>
2015-10-31 23:57:10 -0400

    remove cursor

--- aw_helpers.py
+++ aw_helpers.py
@@ -28,6 +28,7 @@ def upload_to_s3(filepath, targetpath):
     conn = tinys3.Connection(access_key , secret_key, tls=True) #, endpoint = 's3-external-1.amazonaws.com')
     f = open(filepath,'rb')
     conn.upload(targetpath,f, bucket)
+    conn.close()
 
 
 def create_redshift_conn(*args,**kwargs):
@@ -42,7 +43,7 @@ def create_redshift_conn(*args,**kwargs):
         print(err)
 
 
-def copy_to_redshift(filename, table, delim='\\t'):
+def copy_to_redshift_stg(filename, table, delim='\\t'):
     """ insert file to existing table in redshift """
     access_key = settings['s3']['access_key']
     secret_key = settings['s3']['secret_key']
@@ -51,53 +52,15 @@ def copy_to_redshift(filename, table, delim='\\t'):
     con = create_redshift_conn()
     cur = con.cursor()
     sql = """
-    copy %s
+    TRUNCATE table;
+    COPY %s
     FROM '%s'
     credentials 'aws_access_key_id=%s;aws_secret_access_key=%s'
     delimiter '%s'
     ;
-    """ % (table, filepath, access_key, secret_key, delim)
-    cur.execute(sql)
+    """
+    args = (table, filepath, access_key, secret_key, delim)
+    cur.execute(sql, args)
     con.commit()
-
-
-"""
-ADD JAR /home/andywon/projects/udfs/brickhouse/target/brickhouse-0.7.1-SNAPSHOT.jar;
----CREATE TEMPORARY FUNCTION collect AS 'brickhouse.udf.collect.CollectUDAF';
-CREATE TEMPORARY FUNCTION to_json AS 'brickhouse.udf.json.ToJsonUDF';
-CREATE TEMPORARY FUNCTION combine AS 'brickhouse.udf.collect.CombineUDF';
-
-select 
-ev.visitor_id,
-ev.visit_id,
-count(distinct CASE WHEN ev.event = 'view' and ev.context = 'pdp' THEN get_json_object(ev.properties, '$.path') END) as unique_pdp_views,
-count(distinct CASE WHEN ev.event = 'view' and ev.context = 'catalog-index' THEN get_json_object(ev.properties, '$.path') END) as unique_catalog_views,
-count(distinct CASE WHEN ev.event = 'view' and ev.context = 'serp' THEN get_json_object(ev.properties, '$.path') END) as unique_searches,
-count(distinct CASE WHEN ev.event = 'view' and ev.context = 'bdp' THEN get_json_object(ev.properties, '$.path') END) as unique_brand_views,
-MAX(CASE WHEN ev.event = 'view' AND ev.context = 'shop-home' THEN '1-true' ELSE '0-false' END) as show_home,
-MAX(CASE WHEN ev.event = 'view' AND ev.context = 'brand-index' THEN '1-true' ELSE '0-false' END) as brand_index,
-MAX(CASE WHEN ev.event = 'view' AND ev.context = 'cart' THEN '1-true' ELSE '0-false' END) as cart_viewed,
-MAX(CASE WHEN ev.event = 'view' AND ev.context = 'checkout' THEN '1-true' ELSE '0-false' END) as checkout_view,
-exp.experiments
-from event_raw ev
-JOIN(
-SELECT visitor_id, visit_id, collect(event, get_json_object(properties, '$.variant')) as experiments
-from event_raw
-where context = 'experiment'
-and dt = '2015-10-20'
-group by visitor_id, visit_id
-) exp
-ON ev.visitor_id = exp.visitor_id and ev.visit_id = exp.visit_id
-where ev.dt = '2015-10-20'
----and context = 'experiment'
-group by ev.visitor_id, ev.visit_id, exp.experiments
-;
-
-select visitor_id, visit_id, collect(if(context='experiment', explode(event, get_json_object(properties, '$.variant')), NULL))
-from event_raw
-where visitor_id = 'fffc825d-84f9-4113-a8ae-299a10095b1a'
-and visit_id = 8
-and dt = '2015-10-20'
-group by visitor_id, visit_id
-;
-"""
+    con.close()
+    cur.close()
--- aw_queries.py
+++ aw_queries.py
@@ -1,5 +1,13 @@
 hive_session_analytics = """
-
+    ADD JAR /home/klai/projects/warehouse_and_analytics/warehouse_crons/lib/HiveSwarm-1.0-SNAPSHOT.jar;
+    create temporary function user_agent_parser as 'com.livingsocial.hive.udf.UserAgentParser';
+    ADD JAR /home/andywon/projects/udfs/brickhouse/target/brickhouse-0.7.1-SNAPSHOT.jar;
+    CREATE TEMPORARY FUNCTION collect AS 'brickhouse.udf.collect.CollectUDAF';
+    CREATE TEMPORARY FUNCTION to_json AS 'brickhouse.udf.json.ToJsonUDF';
+    INSERT OVERWRITE LOCAL DIRECTORY '/home/andywon/projects/session_analytics_test/%s'
+    ROW FORMAT DELIMITED
+    FIELDS TERMINATED BY '\\t'
+    LINES TERMINATED BY '\\n'
     SELECT ws.visitor_id, ws.visit_id, to_utc_timestamp(from_unixtime(ws.visit_start), 'America/New_York') as visit_start, to_utc_timestamp(from_unixtime(ws.visit_end), 'America/New_York') as visit_end, ws.num_pages, ws.customer_ids[0], ws.landing_url, ws.exit_url,
     ws.ecom_session_revenue, ws.rebillable_sub_session_revenue, ws.gift_sub_session_revenue, ws.giftcard_session_revenue, ws.ecom_session_units, ws.rebillable_sub_session_units, ws.gift_sub_session_units, ws.giftcard_session_units,
     CASE
@@ -80,3 +88,86 @@ hive_session_analytics = """
     AND ws.day >= %s AND ws.day <= %s
     ;
     """ 
+
+session_analytics_stg_to_prod = """
+    WITH box_invervals AS
+    (
+      SELECT a.country,
+             customer_id,
+             CASE
+               WHEN subscription_box_number = 1 THEN d.created_at_time
+               ELSE f_start.date_value
+             END AS int_start,
+             f_end.date_value + 10 AS int_end,
+             c.vertical_id,
+             a.subscription_box_number
+      FROM f_box_subscription_profile a
+        JOIN d_customer b ON a.customer_key = b.customer_key
+        JOIN d_vertical c ON c.vertical_key = a.vertical_key
+        JOIN d_subscription d ON d.subscription_key = a.subscription_key
+        JOIN d_cycle e ON e.cycle_key = a.shipping_cycle_key
+        JOIN d_date f_end ON f_end.date_key = e.end_date_key
+        JOIN d_date f_start ON f_start.date_key = e.start_date_key
+      WHERE a.is_latest_version = 1
+      AND   a.box_payment_status = 'paid'
+    ),
+
+    sub_status AS
+    (
+      SELECT ss.visitor_id,
+             ss.visit_id,
+             ss.customer_id,
+             MAX(CASE WHEN ss.visit_date BETWEEN fb.int_start AND fb.int_end THEN '2-active' WHEN fb.vertical_id IS NULL THEN '0-never' ELSE '1-cancelled' END) AS womens_status,
+             MAX(CASE WHEN ss.visit_date BETWEEN fb_m.int_start AND fb_m.int_end THEN '2-active' WHEN fb_m.vertical_id IS NULL THEN '0-never' ELSE '1-cancelled' END) AS mens_status
+      FROM tmp.stg_session_analytics ss
+        LEFT JOIN box_invervals fb ON fb.int_start < ss.visit_date AND fb.customer_id = ss.customer_id AND fb.country = 'US' AND fb.vertical_id = 1
+        LEFT JOIN box_invervals fb_m ON fb_m.int_start < ss.visit_date AND fb_m.customer_id = ss.customer_id AND fb_m.country = 'US' AND fb_m.vertical_id = 2
+      where trim(ss.customer_id) not like ''
+      and ss.customer_id is not null
+      GROUP BY ss.visitor_id,
+               ss.visit_id,
+               ss.customer_id
+    )
+    stg.visitor_id,
+    stg.visit_id,
+    stg.visit_date,
+    stg.visit_start,
+    stg.visit_end,
+    stg.num_pages,
+    stg.customer_id,
+    stg.entry_page,
+    stg.exit_page,
+    stg.ecom_session_revenue,
+    stg.rebillable_sub_session_revenue,
+    stg.gift_sub_session_revenue,
+    stg.giftcard_session_revenue,
+    stg.ecom_session_units,
+    stg.rebillable_sub_session_units,
+    stg.gift_sub_session_units,
+    stg.giftcard_session_units,
+    stg.source,
+    stg.ua_family,
+    stg.os_family,
+    stg.device,
+    stg.unique_pdp_views,
+    stg.unique_catalog_views,
+    stg.unique_searches,
+    stg.unique_brand_views,
+    stg.shop_home,
+    stg.brand_index,
+    stg.cart_viewed,
+    stg.box_viewed,
+    stg.samples_viewed,
+    stg.checkout_view,
+    stg.experiments,
+    (stg.visit_end - stg.visit_start) as time_spent_sec,
+    --entry_page_type,
+    --exit_page_type,
+    sub.womens_status,
+    sub.mens_status
+    INTO tmp.session_analytics
+    FROM tmp.stg_session_analytics stg
+    JOIN sub_status sub
+    ON stg.visitor_id = sub.visitor_id AND stg.visit_id = sub.visit_id
+    ;
+    """

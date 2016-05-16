ae3b467351ecc46d72d89f5b7fffae993a2568ea
Andy Won <andy.won@birchbox.com>
2016-05-16 14:16:49 -0400

    check hourly

--- check_event_data.sh
+++ check_event_data.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 # yesterday's date
-dt=$(date --date yesterday "+%Y-%m-%d")
+dt=$(date --date today "+%Y-%m-%d")
 
 # file count of yesterday's weborders bucket. We should have 48 every day
-x=$(hdfs dfs -count /event/weborders/$dt)
-num_files=$(echo $x | grep -o " [0-9]. ")
+x=$(hdfs dfs -ls /event/weborders/$dt)
+num_files=$(echo $x | grep -o "tmp" | wc -l)
+y=2
 
 # Message to send if a number != 48 is found
-error="Oh noes! Something might be wrong with the event data. There should be 48 files in the /event/weborders directory, but I only see $num_files"
+error="Oh noes! Something might be wrong with the event data. I don't see 2 tmp files in hdfs dfs -ls /event/weborders/$dt"
 
+# Email list
+send_list=$(echo andy@birchbox.com klai@birchbox.com klai@birchbox.com girish@birchbox.com ethan.rosenthal@birchbox.com junia.zhang@birchbox.com jessica.kahn@birchbox.com matt.negrin@birchbox.com )
 
-if echo $x | grep --quiet "^[1-9] 48 " ; then
+if [ $num_files == $y ] ; then
 	:
 else
-	echo $error | mail -s "Possible Event Data Issue for $dt" dsai@birchbox.com
+	echo $error | mail -s "Possible Event Data Issue for $dt" $send_list
 fi
